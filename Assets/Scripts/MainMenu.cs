4542d8062ba0cfe839e1c5585d805f767ed3224a
Trufafa <illmakeitworkthistime@gmail.com>
2025-08-01 16:31:26 +0100

    Added UI Button Sprites and a Font. Removed the Options Menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,25 +4,12 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string firstLevel;
-    [SerializeField] Canvas startMenuCanvas;
-    [SerializeField] Canvas optionMenuCanvas;
     public void OnStartClick()
     {
         SceneManager.LoadScene("firstLevel");
     }
-    public void OnOptionsClick()
-    {
-        startMenuCanvas.gameObject.SetActive(false);
-        optionMenuCanvas.gameObject.SetActive(true);
-    }
     public void OnExitClick()
     {
         Application.Quit();
-    }
-
-    public void OnExitOptionsClick()
-    {
-        optionMenuCanvas.gameObject.SetActive(false);
-        startMenuCanvas.gameObject.SetActive(true);
     }
 }
