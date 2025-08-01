3ecf4ff4ec9c9502c313b67b2b65c454562ccd1e
Trufafa <illmakeitworkthistime@gmail.com>
2025-08-01 19:45:11 +0100

    Removed the Interactive and PlayerInteraction scripts and bettered the Lever script to fix some bugs

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -8,17 +8,33 @@ public class Lever : MonoBehaviour
     [SerializeField] SpriteRenderer leverSpriteRenderer;
     [SerializeField] UnityEvent OnLeverOff;
     [SerializeField] UnityEvent OnLeverOn;
+    [SerializeField] KeyCode interactKey;
     bool isFlipped = false;
+    [SerializeField] Rigidbody2D rb2d;
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        rb2d.sleepMode = RigidbodySleepMode2D.NeverSleep;
+        if (Input.GetKeyDown(interactKey) && rb2d != null)
+        {
+            LeverFlipped();
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        rb2d.sleepMode = RigidbodySleepMode2D.StartAwake;
+    } 
     public void LeverFlipped()
     {
         if (!isFlipped)
         {
+            Debug.Log("aaaaaaa");
             isFlipped = true;
             leverSpriteRenderer.sprite = flippedLeverSprite;
             OnLeverOn.Invoke();
         }
         else
         {
+            Debug.Log("pppppppp");
             isFlipped = false;
             leverSpriteRenderer.sprite = defaultLeverSprite;
             OnLeverOff.Invoke();
